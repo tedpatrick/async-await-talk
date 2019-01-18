c3669caa5f8351f51e684610849bc59f1a2c4594
Ted Patrick <ted@light.ly>
2019-01-17 19:57:47 -0600

    add preso and examples

---
+++ s1.js
@@ -0,0 +1,15 @@
+async function f() {
+    console.log( "ready" );
+
+    const timeout = (ms) => new Promise( (res) => setTimeout(res, ms) )
+
+    await timeout(1000);
+    console.log( "after 1000ms" );
+
+    await timeout(2000);
+    console.log( "after 2000ms" );
+
+    await timeout(1000);
+    console.log( "after 1000ms --> done!" );
+}
+f();
---
+++ s5.js
@@ -0,0 +1,11 @@
+async function f() {
+    console.log( "ready" );
+    const timeout = (ms) => new Promise( (res, rej) => setTimeout(()=>{rej("BOOM")}, ms) )
+    try{
+        await timeout(3000);
+        console.log("COMPLETE");
+    } catch(error){
+        console.log("ERROR",error);
+    }
+}
+f();
